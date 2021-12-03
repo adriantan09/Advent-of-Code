dbb5324784c5926453949373913eeddbac685570
Tan <atan3@deloitte.com.au>
2021-12-03 16:57:46 +1100

    day3 task1 complete

--- day3/main.js
+++ day3/main.js
@@ -2,9 +2,9 @@ import fs from 'fs';
 import part1 from './part1.js';
 import part2 from './part2.js';
 
-let instructions = fs.readFileSync('example.txt', 'utf-8');
+let binaryStrings = fs.readFileSync('example.txt', 'utf-8');
 
-instructions = instructions.split('\r\n');
+binaryStrings = binaryStrings.split('\r\n');
 
-console.log('', part1(instructions));
-console.log('', part2(instructions));
+console.log('Power Consumption:', part1(binaryStrings));
+// console.log('', part2(instructions));
--- day3/part1.js
+++ day3/part1.js
@@ -1,5 +1,26 @@
-const part1 = (instructions) => {
-  return 0;
+const part1 = (binaryStrings) => {
+  let gammaRate = '';
+  let epsilonRate = '';
+  // Assumes that all binary strings provided are the same length
+  for (let col = 0; col < binaryStrings[0].length; col++) {
+    let oneCount = 0;
+    let zeroCount = 0;
+    for (let row = 0; row < binaryStrings.length; row++) {
+      if (parseInt(binaryStrings[row].charAt(col))) {
+        oneCount++;
+      } else {
+        zeroCount++;
+      }
+    }
+    if (oneCount > zeroCount) {
+      gammaRate = gammaRate.concat('1');
+      epsilonRate = epsilonRate.concat('0');
+    } else {
+      gammaRate = gammaRate.concat('0');
+      epsilonRate = epsilonRate.concat('1');
+    }
+  }
+  return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2);
 };
 
 export default part1;

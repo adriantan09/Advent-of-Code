31bbae2d226e27a1590df517039f4fab7c77d4fc
Adrian Tan <adriantan@Adrians-MBP.fritz.box>
2021-12-13 10:44:51 +1100

    completed day5 task1

--- day5/main.js
+++ day5/main.js
@@ -2,9 +2,73 @@ import fs from 'fs';
 import part1 from './part1.js';
 import part2 from './part2.js';
 
-let instructions = fs.readFileSync('example.txt', 'utf-8');
+let input = fs.readFileSync('input.txt', 'utf-8');
+input = input.split('\n');
 
-instructions = instructions.split('\r\n');
+const filterInput = (input) => {
+  const hLines = [];
+  const vLines = [];
+  input.forEach((line) => {
+    line = line.match(/\d+/g).map((num) => parseInt(num, 10));
 
-console.log('', part1(instructions));
-console.log('', part2(instructions));
+    // part 1: only consider horizontal and vertical lines
+    line = { startX: line[0], startY: line[1], endX: line[2], endY: line[3] };
+
+    if (line.startX === line.endX) {
+      vLines.push(line);
+    } else if (line.startY === line.endY) {
+      hLines.push(line);
+    }
+  });
+  return { horizontal: hLines, vertical: vLines };
+};
+
+input = filterInput(input);
+
+const matrix = new Array(1000).fill(0).map(() => new Array(1000).fill(0));
+
+input.vertical.forEach((line) => {
+  if (line.startY < line.endY) {
+    for (let i = line.startY; i <= line.endY; i++) {
+      matrix[i][line.startX]++;
+    }
+  } else {
+    for (let i = line.startY; i >= line.endY; i--) {
+      matrix[i][line.startX]++;
+    }
+  }
+});
+
+input.horizontal.forEach((line) => {
+  if (line.startX < line.endX) {
+    for (let i = line.startX; i <= line.endX; i++) {
+      matrix[line.startY][i]++;
+    }
+  } else {
+    for (let i = line.startX; i >= line.endX; i--) {
+      matrix[line.startY][i]++;
+    }
+  }
+});
+
+// console.log('instructions:', input);
+
+const countOverlap = (matrix) => {
+  let overlap = 0;
+  matrix.forEach((row) => {
+    row.forEach((col) => {
+      if (col >= 2) overlap++;
+    });
+  });
+  return overlap;
+};
+
+const printMatrix = (matrix) => {
+  matrix.forEach((row) => {
+    console.log(row.join(' '));
+  });
+};
+// printMatrix(matrix);
+
+console.log('lines that overlap:', countOverlap(matrix));
+// console.log('', part2(instructions));

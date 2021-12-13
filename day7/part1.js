a8319f3dae92b8ad53b74bdacc9e9155cf784574
Adrian Tan <adriantan@Adrians-MBP.fritz.box>
2021-12-13 19:49:00 +1100

    completed day7 task1

--- day7/part1.js
+++ day7/part1.js
@@ -1,5 +1,19 @@
-const part1 = (instructions) => {
-  return 0;
+const part1 = (crabs) => {
+  crabs.sort((c1, c2) => c1 - c2);
+
+  const half = Math.floor(crabs.length / 2);
+  let median = 0;
+
+  if (crabs.length % 2 === 0) {
+    median = (crabs[half - 1] + crabs[half]) / 2;
+  } else {
+    median = crabs[half];
+  }
+
+  let fuel = 0;
+  crabs.forEach((crab) => (fuel += Math.abs(crab - median)));
+
+  return fuel;
 };
 
 export default part1;

591ef3631988b52bf7904debc6ee1667e956a612
Adrian Tan <adrian.tan09@gmail.com>
2021-12-25 23:40:38 +1100

    completed day10 part 1

--- day10/main.js
+++ day10/main.js
@@ -2,9 +2,9 @@ import fs from 'fs';
 import part1 from './part1.js';
 import part2 from './part2.js';
 
-let instructions = fs.readFileSync('example.txt', 'utf-8');
+let instructions = fs.readFileSync('input.txt', 'utf-8');
 
-instructions = instructions.split('\r\n');
+instructions = instructions.split('\n');
 
-console.log('', part1(instructions));
-console.log('', part2(instructions));
+console.log('Total syntax error:', part1(instructions));
+// console.log('', part2(instructions));
--- day10/part1.js
+++ day10/part1.js
@@ -1,5 +1,44 @@
-const part1 = (instructions) => {
-  return 0;
+const openingBrackets = ['(', '[', '{', '<']
+const closingBrackets = [')', ']', '}', '>']
+
+const part1 = (lines) => {
+  let totalError = 0
+  lines.forEach(line => {
+    const bracket = checkInvalidBracket(line)
+    switch (bracket) {
+      case ')':
+        totalError += 3
+        break;
+      case ']':
+        totalError += 57
+        break;
+      case '}':
+        totalError += 1197
+        break;
+      case '>':
+        totalError += 25137
+        break;
+    }
+  })
+  return totalError
 };
+
+const checkInvalidBracket = (line) => {
+  const bracketStack = []
+  for (let char of line) {
+    if (openingBrackets.includes(char)) {
+      bracketStack.push(char)
+    } else {
+      const openingIdx = openingBrackets.findIndex(e => e === bracketStack[bracketStack.length - 1])
+      const closingIdx = closingBrackets.findIndex(e => e === char)
+      if (openingIdx === closingIdx) {
+        bracketStack.pop()
+      } else {
+        return char
+      }
+    }
+  }
+  return null
+}
 
 export default part1;

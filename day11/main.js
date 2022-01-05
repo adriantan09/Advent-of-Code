79d1051fb096b8259a7b7ecff60395fe0a894f0a
Adrian Tan <adrian.tan09@gmail.com>
2022-01-06 08:43:35 +1100

    added other examples / inputs

--- day11/main.js
+++ day11/main.js
@@ -1,10 +1,8 @@
 import fs from 'fs';
 import part1 from './part1.js';
-import part2 from './part2.js';
+// import part2 from './part2.js';
 
-let instructions = fs.readFileSync('example.txt', 'utf-8');
-
-instructions = instructions.split('\r\n');
+let instructions = fs.readFileSync('example-simple.txt', 'utf-8');
 
 console.log('', part1(instructions));
-console.log('', part2(instructions));
+// console.log('', part2(instructions));
--- day11/part1.js
+++ day11/part1.js
@@ -1,5 +1,47 @@
+const input2Array = (input) => {
+  return input
+    .split('\n')
+    .map(e => {
+      return e.split('').map(e => parseInt(e, 10))
+    });
+}
+
+const countFlashes = (array, steps) => {
+  for (let step = 0; step < steps; step++) {
+    for (let row = 0; row < array[0].length; row++) {
+      for (let col = 0; col < array.length; col++) {
+        array[row][col]++
+
+        // Flashes
+        if (array[row][col] > 9) {
+          const boundaries = findBoundaries(array, row, col)
+          // console.log(`(${row}, ${col}): ${boundaries}`)
+          boundaries.forEach(e => array[e[0]][e[1]]++)
+          array[row][col] = 0
+        }
+      }
+    }
+  }
+  return array
+}
+
+const findBoundaries = (array, row, col) => {
+  const boundaries = []
+  for (let py = (row > 0 ? -1 : 0); py <= (row < array.length - 1 ? 1 : 0); py++) {
+    for (let px = (col > 0 ? -1 : 0); px <= (col < array[0].length - 1 ? 1 : 0); px++) {
+      if ((py != 0 || px != 0) && array[row][col] < 9) {
+        boundaries.push([row + py, col + px])
+      }
+    }
+  }
+  return boundaries
+}
+
 const part1 = (instructions) => {
-  return 0;
+  let  array = input2Array(instructions);
+  array = countFlashes(array, 1)
+  console.log(array.join('\n'))
+  // return 0;
 };
 
 export default part1;
